2e540661763b2eb7be6a9000d097ad9d9c46f082
ken <block24block@gmail.com>
2021-05-16 16:38:11 +0800

    setup db test

---
+++ mongist.go
@@ -0,0 +1,5 @@
+package mongist
+
+func Statistic() {
+
+}
---
+++ mongist_test.go
@@ -0,0 +1,56 @@
+package mongist
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var client *mongo.Client
+
+func TestMain(m *testing.M) {
+	uri := os.Getenv("MONGIST_URI")
+
+	newClient, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = newClient.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer newClient.Disconnect(ctx)
+
+	client = newClient
+	m.Run()
+}
+
+func TestPing(t *testing.T) {
+	err := client.Ping(aCtx(), readpref.Primary())
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDatabases(t *testing.T) {
+	databases, err := client.ListDatabaseNames(aCtx(), bson.M{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(databases)
+}
+
+func aCtx() context.Context {
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	return ctx
+}
